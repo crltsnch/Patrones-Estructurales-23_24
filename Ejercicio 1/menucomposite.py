aa02f60e71d2195475f717a69d98b5ecfdc29eb7
crltsnch <crltsnch@myuax.com>
2023-11-19 10:40:22 +0100

    codigo cliente

--- Ejercicio 1/menucomposite.py
+++ Ejercicio 1/menucomposite.py
@@ -165,12 +165,21 @@ class CompositeComboPareja(Menu):
         return precio_combo1 + precio_combo2
 
 
-def client_code(component: List[Menu]) -> None:   #menu simple
+def client_code(mensaje, opciones) -> None:   #menu simple
     """
-    El código del cliente funciona con todos los componentes a través de la interfaz base. Vamos a pedirle 
+    El código del cliente funciona con todos los componentes a través de la interfaz base. Vamos a pedirle al cliente si quiere crear su menu o elegir uno ya hecho.
     """
-    print(f"Nombre: {component.nombre()}")
-    print(f"Precio: {component.precio()}")
+    while True:
+        try:
+            eleccion = int(input(mensaje))
+            if eleccion in opciones:
+                return eleccion
+            else:
+                print("Opción no disponible")
+                
+        except ValueError:
+            print("Opción no disponible")
+    
 
 
 
