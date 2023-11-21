3a8ee446b1e2c2b7a28a86f75ef5f6f643bd9289
crltsnch <crltsnch@myuax.com>
2023-11-21 12:11:33 +0100

    test entrante

--- Ejercicio 1/test.py
+++ Ejercicio 1/test.py
@@ -7,5 +7,8 @@ from composite import CompositeCombo, CompositeComboCompuesto
 '''Pruebas unitarias que cubran casos básicos.
 Cobertura de casos de borde y validación de correcta funcionalidad en diferentes escenarios'''
 
-
-
+class TestMenus(unittest.TestCase):
+    def test_entrante(self):
+        entrante = Entrante("salsa cesar", 3.5)
+        self.assertEqual(entrante.mostrar, "Entrante: salsa cesar - Precio: 3.5")
+        
