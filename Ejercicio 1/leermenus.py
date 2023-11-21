b20b2db156bbfde4398ea672c6f57b1d769fbb9a
crltsnch <crltsnch@myuax.com>
2023-11-21 13:32:15 +0100

    reconstruir

--- Ejercicio 1/leermenus.py
+++ Ejercicio 1/leermenus.py
@@ -7,3 +7,12 @@ def leer_pizzas_desde_csv(archivo_csv):
         for row in reader:
             menus.append(row)
     return menus
+
+def reconstruir_menu(datos):
+    menu = f"ID: {datos['id']}\n"
+    menu += f"Nombre: {datos['nombre']}\n"
+    menu += f"Entrante: {datos['entrante']}\n"
+    menu += f"Pizza: {datos['pizza']}\n"
+    menu += f"Bebida: {datos['bebida']}\n"
+    menu += f"Postre: {datos['postre']}\n"
+    return menu
