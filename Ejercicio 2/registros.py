dccfeefa7f44a5e97f160c7e29e6bc97cfa2ffa8
crltsnch <crltsnch@myuax.com>
2023-11-27 23:08:32 +0100

    funcion logger

--- Ejercicio 2/registros.py
+++ Ejercicio 2/registros.py
@@ -13,3 +13,19 @@ def registrar(log_entry: dict) -> None:
             writer.writeheader()
             csv_existe = True
         writer.writerow(log_entry)
+
+def logger(func):
+    def wrapper(*args, **kwargs):
+        log_entry = {
+            "timestamp": datetime.datetime.now().timestamp(),
+            "usuario": kwargs["usuario"],
+            "accion": func.__name__,
+            "tipo": args[0].__class__.__name__,
+            "info_extra": args[0].nombre
+        }
+        if args:
+            log_entry["usuario"] = args[0]
+
+        registrar(log_entry)
+        return func(*args, **kwargs)
+    return wrapper
