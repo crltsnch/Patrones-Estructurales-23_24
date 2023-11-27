5a2a418d42d3a0d3a727880b0008eb9ae330e9c4
crltsnch <crltsnch@myuax.com>
2023-11-27 21:45:13 +0100

    json que guarde los archivos que creamos

--- Ejercicio 2/composite-estructura.py
+++ Ejercicio 2/composite-estructura.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List, Union, Dict
 import json
-
+import os
 
 class Component(ABC):
     """
@@ -153,8 +153,21 @@ def client_code(component: Component) -> None:
 
 
 def guardar_en_json(data: Union[str, int, float, bool, None, Dict], filename: str) -> None:
-    with open('archivo.json', 'w') as json_file:
-        json.dump(data, json_file, indent=2)
+    if os.path.exists(filename):
+        # Si el archivo JSON ya existe, cargamos los datos existentes
+        with open(filename, 'r') as json_file:
+            existing_data = json.load(json_file)
+    else:
+        # Si el archivo no existe, creamos un diccionario vacío
+        existing_data = {}
+
+    # Asignamos el nuevo dato con una clave única (puedes adaptar esto según tus necesidades)
+    key = f"{data['type']}_{data['nombre']}"
+    existing_data[key] = data
+
+    # Guardamos el diccionario actualizado en el archivo JSON
+    with open(filename, 'w') as json_file:
+        json.dump(existing_data, json_file, indent=2)
 
 
 if __name__ == "__main__":
