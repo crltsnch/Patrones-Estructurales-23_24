030be198e82acb424c053383d410c4f1440a7bc6
crltsnch <crltsnch@myuax.com>
2023-11-28 21:27:13 +0100

    test

--- Ejercicio 2/test.py
+++ Ejercicio 2/test.py
@@ -13,4 +13,15 @@ class TestEstructuraComposite(unittest.TestCase):
         self.ruta_carpeta = Carpeta("Documentos")
         self.usuario_prueba = "usuario_prueba"
     
+    def test_acceso(self):
+        documento_confidencial = Documento("Confidencial.txt", "txt", 1000, sensible=True)
+        proxy_documento = Proxy(documento_confidencial)
+
+        with patch('builtins.input', return_value=self.usuario_prueba):
+            proxy_documento.acceder()
+
+        # Verificar que el acceso se ha registrado en el proxy
+        self.assertIn(f"Solicitud de acceso de {self.usuario_prueba} al documento {documento_confidencial.nombre}",
+                      proxy_documento.accesos_registrados)
+        
     
